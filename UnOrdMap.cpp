a69ea498260b02c9d79155c526c79df3561e43e5
Khushiii👩‍💻 <145993686+khushikumari239@users.noreply.github.com>
2025-04-20 21:32:28 +0530

    add new files

---
+++ UnOrdMap.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // i will need a " n " then the " array " then i will need " q " the queries .... later on the " numbers "
+    int n;
+    cin>>n;
+    int arr[n];
+
+    for(int i = 0; i<n; i++) {
+        cin>>arr[i];
+    }
+
+    // PRE-COMPUTE STEP 1
+
+    unordered_map<int , int>mpp;
+    for(int i=0; i<n; i++) {
+        mpp[arr[i]] ++;
+    }
+
+     // Iterate the mapp auto:it
+     for(auto it : mpp ) {
+        cout<<it.first<< "->" <<it.second << endl;
+    }
+
+
+    int q;
+    cin>>q;
+
+    while (q--) {
+        int number;
+        cin>>number;
+
+        //FETCH STEP 2
+
+        cout<<mpp[number] << endl;
+        
+
+
+
+
+    }
+
+
+
+
+    return 0;
+}
